1723879492de0151bba12bf590a0467e4cdc085e
Motko <77466238+Motko222@users.noreply.github.com>
2026-02-27 13:07:00 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 source $path/env
 
 cd ~/snapshotter-lite-v2/mainnet/bds_mainnet_uniswapv3_eth-mainnet/slot-$SLOT
-./build.sh --bds-dsv-mainnet --no-collector --skip-credential-update --data-market-contract-number 1
+./build.sh --bds-dsv-mainnet --skip-credential-update --data-market-contract-number 1
